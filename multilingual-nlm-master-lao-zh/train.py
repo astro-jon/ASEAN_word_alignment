373fb2ec2177c950ed6bfc841513076238d54e6c
Jingshen Zhang <audbut0702@163.com>
2024-07-18 11:47:46 +0800

    update

---
+++ multilingual-nlm-master-lao-zh/train.py
@@ -0,0 +1,67 @@
+# # #!/usr/bin/env python
+# # # # # # # -*- coding: utf-8 -*-
+import os
+import numpy as np
+import argparse
+import pickle
+from logging import getLogger, StreamHandler, FileHandler, basicConfig, INFO, WARNING
+basicConfig(level=WARNING) #Use INFO to print more information
+import sys,os
+sys.path.append(os.getcwd())
+from train_option import global_train_parser
+import warnings
+from utils.train_base_new import check_options, load_data, save_emb, out_wordemb, generate_bacth_idx, preprare_model
+from utils.train_class import Trainer_MT
+import torch
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(parents=[global_train_parser])
+    opt = parser.parse_args()
+    if opt.subword:
+        assert opt.share_vocab != 0, "Subwords are not used"
+    opt.save_dir = opt.save_dir +'-'.join([str(x) for x in opt.lang_class])
+    logger = getLogger('Log')
+    handler = StreamHandler()
+    logger.addHandler(handler)
+    if (os.path.isdir(opt.save_dir)):
+        message = 'Directory ' + "'" + opt.save_dir + "'" + ' already exists.'
+        warnings.warn(message)
+    else:
+        os.mkdir(opt.save_dir)
+    fh = FileHandler(opt.save_dir + '/test.log')
+    logger.addHandler(fh)
+    check_options(opt)
+    with open(opt.save_dir+"/options.pkl", 'wb') as f:
+        pickle.dump(opt, f)  # N_idiom, N_sample, s_len, dim
+    file_name = opt.save_dir + '/' + opt.data
+    logger.info("Save model as: "+ file_name)
+    dataset, vocab_dict = load_data("data/"+opt.data, logger)
+    dataset.batch_idx_list = generate_bacth_idx(dataset.lengths, dataset.lengths_mono, dataset.lengths_multi, opt.batch_size, logger)
+    model, dataset, vocab_dict = preprare_model(opt, dataset, vocab_dict,logger)
+    logger.info(model)
+    model.set_device(opt.gpu)
+    if opt.gpu:
+        model.to('cuda')
+    if torch.cuda.device_count() > 1:
+        from models.new_models import MyDataParallel
+        logger.critical("Use multiple GPUs")
+        model = MyDataParallel(model)
+
+    trainer = Trainer_MT(model, dataset, file_name, vocab_dict, opt)
+    trainer.Set_Optimiser(model, opt.opt_type, opt.learning_rate_SGD)
+    bestmodel = trainer.main(model, opt.epoch_size, opt.stop_threshold, opt.remove_models, opt.early_stop)
+    logger.info("save embeddings")
+    bestmodel.eval()
+    if opt.pretrained_emb:
+        param_a = bestmodel.embedding_weight.a.data.cpu().numpy()
+        np.save(file_name + ".param_a", param_a)
+        param_b = bestmodel.embedding_weight.b.data.cpu().numpy()
+        np.save(file_name + ".param_b", param_b)
+    # with torch.no_grad(): #save word embeddings
+    #     for lang in range(bestmodel.lang_size):
+    #         emb_weight = bestmodel.embedding_weight(lang)
+    #         vocab2emb = out_wordemb(vocab_dict.id2vocab[lang], emb_weight)
+    #         save_name = file_name + '.lang' + str(lang) + '.vec'
+    #         save_emb(vocab2emb, opt.emb_size, save_name)
+
+
---
+++ multilingual-nlm-master-lao-zh/train_option.py
@@ -0,0 +1,171 @@
+import argparse
+
+global_train_parser = argparse.ArgumentParser(add_help=False)
+
+global_train_parser.add_argument(
+    '-gpu',
+    default=True,
+    action='store_true',
+    help ='Use GPU (recomended)')
+
+global_train_parser.add_argument(
+    '-dr_rate',
+    default=0.5,
+    type=float,
+    help='dropout rate applied to LMs (default: 0.5)')
+
+global_train_parser.add_argument(
+    '-n_layer',
+    default=1,
+    type=int,
+    help='number of layers (default: 1)')
+
+global_train_parser.add_argument(
+    '-emb_size',
+    default=500,
+    type=int,
+    help='embedding size (default: 500)')
+
+global_train_parser.add_argument(
+    '-h_size',
+    default=500,
+    type=int,
+    help='hidden state size (default: 500)')
+
+global_train_parser.add_argument(
+    '-batch_size',
+    default=16,
+    type=int,
+    help='batch size (default: 16)')
+
+global_train_parser.add_argument(
+    '-epoch_size',
+    type=int,
+    default= 200,
+    help='number of epochs')
+
+global_train_parser.add_argument(
+    '-save_dir',
+    type=str,
+    default='Result_lao_zh_AveWord_InfoNCE_BatchSize16_Proj_tau0.5_RELU_1_',
+    help="directory where models and embeddings are saved")
+
+global_train_parser.add_argument(
+    '-data',
+    type=str,
+    default='lao-zh.para',
+    help='preprocessed data name')
+
+global_train_parser.add_argument(
+    '-opt_type',
+    default="Adam",
+    choices=['SGD','Adam'],
+    help='optimizer (SGD or Adam) (default: Adam)')
+
+global_train_parser.add_argument(
+    '-learning_rate_SGD',
+    default=1.0,
+    type=float,
+    help='learning rate for SGD (default: 1.0)')
+
+global_train_parser.add_argument(
+    '-stop_threshold',
+    default = float('inf'),
+    type=float,
+    help="threshold for unsupervised early stopping (the default is 'inf' (= disable early stopping)")
+
+global_train_parser.add_argument(
+    '-early_stop',
+    action='store_true',
+    help='enable early stop using validation data'
+    )
+
+global_train_parser.add_argument(
+    '-remove_models',
+    action='store_true',
+    default = 'disabled',
+    help='save the best/last model only (default: disabled)')
+
+global_train_parser.add_argument(
+    '-seed',
+    default=1,
+    type=int,
+    help='random seed (default: 0)')
+
+global_train_parser.add_argument(
+    '-save_point',
+    default=1,
+    type=int,
+    help='how often the model is saved (default: 1 epoch)')
+
+global_train_parser.add_argument(
+    '-eval_dict',
+    default=['./data/lao-Chinese-dic-Glosbe.txt'],
+    nargs='+', type=str)
+
+global_train_parser.add_argument(
+    '-dico_max_size',
+    type=int,
+    default=3000,
+    help='source train data path')
+
+global_train_parser.add_argument(
+    '-lang_class',
+    nargs='+',
+    type=int,
+    default=[0, 1],
+    help='decoder id'
+)
+
+global_train_parser.add_argument(
+    '-pretrained_emb',
+    nargs='+',
+    type=str,
+    help='language class based on word order (e.g SVO, SOV, VSO, etc.)'
+)
+
+global_train_parser.add_argument(
+    '-share_vocab',
+    choices=[0, 3, 4],
+    default=3,
+    type=int
+)
+
+global_train_parser.add_argument(
+    '-swemb_size',
+    type=int,
+    help='backward training'
+)
+global_train_parser.add_argument(
+    '-enc_dec_layer',
+    nargs='+',
+    default=[1, 1],
+    type=int,
+    help='number of layers of encoder and decoder'
+)
+
+global_train_parser.add_argument(
+    '-subword',
+    type=str,
+    nargs='+',
+    default=['./SP/lao_subwords.txt', './SP/zn_subwords(lao).txt'],
+    help='subword file')
+
+global_train_parser.add_argument(
+    '-dict_srclang',
+    type=int,
+    default=0,
+)
+
+global_train_parser.add_argument(
+    '-dict_tgtlangs',
+    type=int,
+    nargs='+',
+    default=None
+)
+
+global_train_parser.add_argument(
+    '-loss_type',
+    default=0,
+    type=int,
+    help='0: inter loss; 1: inter+intra loss')
